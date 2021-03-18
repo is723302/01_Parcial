238becdb55a4d10144d11e9c25c3aef35a05c1ec
Julieta <julietaramosayon@gmail.com>
2021-03-18 14:00:02 -0600

    backend

--- Back-end/tone-analyzer.js
+++ Back-end/tone-analyzer.js
@@ -1,33 +1,34 @@
 const express = require('express')
 const path = require('path');
 const app = express();
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || "0.0.0.0"
 
-// Pass IAM credentials to gain access to API
-const toneAnalyzer = new ToneAnalyzerV3({
-    version: process.env.TONE_ANALYZER_VERSION,
-    authenticator: new IamAuthenticator({
-        apikey: process.env.TONE_ANALYZER_API_KEY,
-    }),
-    serviceUrl: process.env.TONE_ANALYZER_URL,
-});
 
 // POST method route
 app.post('/', function (req, res) {
     // Text to pass to API
-    let text = req.body.userinput
-    console.log("texto:" + text)
+    const text = req.body.text
+    console.log(text)
+
+    const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+
+    // Pass IAM credentials to gain access to API
+    const toneAnalyzer = new ToneAnalyzerV3({
+        version: process.env.TONE_ANALYZER_VERSION,
+        apikey: process.env.TONE_ANALYZER_API_KEY,
+        serviceUrl: process.env.TONE_ANALYZER_URL
+    });
+
     // Object to send to API and return JSON file
     const toneParams = {
-        toneInput: { 'text': text },
-        contentType: 'application/json',
+        toneInput: text,
+        contentType: 'text/plain'
     };
-    
+
     toneAnalyzer.tone(toneParams)
         .then(toneAnalysis => {
             res.send(JSON.stringify(toneAnalysis, null, 2));
@@ -35,8 +36,9 @@ app.post('/', function (req, res) {
         .catch(err => {
             console.log('error:', err);
         });
+
 })
 
-app.listen(PORT,HOST, () => { 
-    console.log(`Server listening on port ${PORT} and host ${HOST}`); 
+app.listen(PORT, HOST, () => {
+    console.log(`Server listening on port ${PORT} and host ${HOST}`);
 })
