3dbb111e6e4a48db801b4ede19b3e1968e0fef35
Julieta <julietaramosayon@gmail.com>
2021-03-18 14:34:32 -0600

    tone analizer functioning

--- Back-end/tone-analyzer.js
+++ Back-end/tone-analyzer.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path');
 const app = express();
 const { IamAuthenticator } = require('ibm-watson/auth');
 require("dotenv").config();
@@ -7,26 +6,30 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || "0.0.0.0"
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // POST method route
 app.post('/', function (req, res) {
     // Text to pass to API
     const text = req.body.text
-    console.log(text)
+    //console.log(text)
 
     const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-
+    
     // Pass IAM credentials to gain access to API
     const toneAnalyzer = new ToneAnalyzerV3({
         version: process.env.TONE_ANALYZER_VERSION,
-        apikey: process.env.TONE_ANALYZER_API_KEY,
-        serviceUrl: process.env.TONE_ANALYZER_URL
+        authenticator: new IamAuthenticator({
+            apikey: process.env.TONE_ANALYZER_API_KEY,
+          }),
+        serviceUrl: process.env.TONE_ANALYZER_URL,
     });
 
     // Object to send to API and return JSON file
     const toneParams = {
-        toneInput: text,
-        contentType: 'text/plain'
+        toneInput: {'text': text},
+        contentType: 'application/json',
     };
 
     toneAnalyzer.tone(toneParams)
