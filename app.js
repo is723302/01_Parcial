78e0917fbac0bfd9084bc3355be38e701cca5037
Julieta <julietaramosayon@gmail.com>
2021-03-17 20:00:12 -0600

    avance front-end

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express')
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || "0.0.0.0"
 
-const router = require('./src/routes')
-
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'src/html/index.html'));
   });
 
-app.use(router)
 
 app.listen(PORT,HOST, () => { 
     console.log(`Server listening on port ${PORT} and host ${HOST}`); 
--- src/routes/home.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const path = require('path');
-
-router.route('/').get((req, res) => {
-    res.sendFile(path.join(__dirname+'../src/html/index.html'));
-  })
-  
-module.exports = router
